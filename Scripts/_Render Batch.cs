1919597f853492ba05893115e7edd0a51c94730f
carlosagarciaii <carlosagarciaii@gmail.com>
2024-10-18 11:51:40 -0500

    Adjusting Render Shorts Button

--- Scripts/_Render Batch.cs
+++ Scripts/_Render Batch.cs
@@ -346,7 +346,7 @@ public class EntryPoint {
         dlog.CancelButton = cancelButton;
         dlog.Controls.Add(cancelButton);
 
-        dlog.Height = 900; //titleBarHeight + okButton.Bottom + 8;
+        dlog.Height = titleBarHeight + okButton.Bottom + 8;
         dlog.ShowInTaskbar = false;
 
         FillTemplateTree();
