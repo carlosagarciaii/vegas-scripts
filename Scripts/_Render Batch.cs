317b8c54cb0417db0ae352cc36d5a0e4973e6bc8
carlosagarciaii <carlosagarciaii@gmail.com>
2024-10-19 15:55:18 -0500

    Adding renaming of output when too long for short

--- Scripts/_Render Batch.cs
+++ Scripts/_Render Batch.cs
@@ -12,6 +12,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 using ScriptPortal.Vegas;
 
