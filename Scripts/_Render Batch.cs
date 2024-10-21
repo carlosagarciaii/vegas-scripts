f985b28d19d9dc2665bee798bb50e6f90efb7093
carlosagarciaii <carlosagarciaii@gmail.com>
2024-10-21 14:51:26 -0500

    Adjusting Dialog Lenght in form for Shorts Length

--- Scripts/_Render Batch.cs
+++ Scripts/_Render Batch.cs
@@ -330,7 +330,7 @@ public class EntryPoint {
         ShortsMaxLength = AddTextControl(dlog,
                                         "Shorts Max Length",
                                         RenderCreateShortsCheckBox.Right + 10,
-                                        200,
+                                        75,
                                         RenderCreateShortsCheckBox.Top,
                                         "60");
 
