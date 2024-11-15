1ec32229bda0c4e78bfeeace9e7886034abeb2f2
carlosagarciaii <carlosagarciaii@gmail.com>
2024-11-15 15:12:46 -0600

    Init Branch SetupLayers

---
+++ Scripts/_Setup Layers.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Drawing;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+using ScriptPortal.Vegas;
+
+
+public class EntryPoint {
+
+
+    ScriptPortal.Vegas.Vegas myVegas = null;
+
+
+}   // EntryPoint
+
