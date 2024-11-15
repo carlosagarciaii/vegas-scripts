7fac7536cb3d4b08514e85e9eb73d9ba6e2ca49a
carlosagarciaii <carlosagarciaii@gmail.com>
2024-11-15 15:23:26 -0600

    Setting up Form

--- Scripts/_Setup Layers.cs
+++ Scripts/_Setup Layers.cs
@@ -17,6 +17,29 @@ public class EntryPoint {
 
     ScriptPortal.Vegas.Vegas myVegas = null;
 
+    public void FromVegas(Vegas myVegas){
+
+        DialogResult result = ShowMainDialog();
+
+
+    }   // FromVegas
+
+    DialogResult ShowMainDialog(){
+        Form dlog = new Form();
+        dlog.Text = "Setup Layers";
+        dlog.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+        dlog.MaximizeBox = false;
+        dlog.StartPosition = FormStartPosition.CenterScreen;
+
+
+
+    }   // ShowMainDialog
+
+    void HandleMainClosing(Object sender, EventArgs args){
+
+
+    }   //  HandleMainClosing
+
 
 }   // EntryPoint
 
