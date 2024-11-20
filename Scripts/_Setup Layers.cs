9673d06777e2789192d4ca09470b3139afe6132f
carlosagarciaii <carlosagarciaii@gmail.com>
2024-11-20 16:39:37 -0600

    Adding Browse Buttons| Updating Default Folder

--- Scripts/_Setup Layers.cs
+++ Scripts/_Setup Layers.cs
@@ -18,7 +18,7 @@ public class EntryPoint {
     ScriptPortal.Vegas.Vegas myVegas = null;
 
     TextBox FileNameBox;
-    string defaultBasePath = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
+    string defaultBasePath = Environment.GetFolderPath(Environment.SpecialFolder.Recent);
     Button BrowseButton;
 
     public void FromVegas(Vegas vegas){
